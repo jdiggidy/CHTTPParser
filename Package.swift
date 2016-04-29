29c5472a2d17900b6d0c07f524b01d3c6ea26a2b
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-04-29 15:54:57 -0500

    Added pkg-config file for http-parser

--- Package.swift
+++ Package.swift
@@ -13,3 +13,14 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 **/
+
+import PackageDescription
+
+let package = Package(
+    name: "CHttpParser",
+    pkgConfig: "http-parser",
+    providers: [
+                   .Brew("http-parser"),
+                   .Apt("libhttp-parser-dev ")
+    ]
+)
